d8cbf4c525d826c848a5683c42ce31a3cc008f62
Emilio Porteros <18100436@ue.edu.pe>
2024-09-29 14:21:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/porterospc1/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.porterospc1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val etEmail: EditText = findViewById(R.id.etEmail)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if(email == "invitado@android.com" && password == "invitado123"){
+                val intent = Intent(this, ListadoActivity::class.java)
+                startActivity(intent)
+            }else{
+                // Credenciales incorrectas, mostrar mensaje de error
+                etEmail.error = "Credenciales incorrectas"
+            }
+        }
+    }
+}
